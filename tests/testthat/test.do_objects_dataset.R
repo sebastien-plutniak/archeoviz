5e346e4782507db74a4f5c2687a0fcff723cdb7e
sebastien-plutniak <sebastien.plutnia@posteo.net>
2022-12-26 14:42:00 +0100

    fix demo_objects_data and test

--- tests/testthat/test.do_objects_dataset.R
+++ tests/testthat/test.do_objects_dataset.R
@@ -7,5 +7,8 @@ test_that(".do_objects_dataset: simulated data", {
   expect_equal(res$notif.text, "notif.objects.ok")
   expect_equal(dim(res$data), c(100, 19))
   expect_equal(colnames(res$data), c("id", "square_x", "square_y", "xmin", "xmax", "ymin", "ymax", "zmin", "zmax", "layer", "object_type", "object_class_size", "year", "location_mode", "x", "y", "z", "square", "layer_color"))
-  expect_equal(c(sapply(res$data[1, ], typeof), use.names=F), c("integer", "integer", "integer", "integer", "double", "integer", "double", "double", "logical", "integer", "character", "character", "integer", "character","integer", "integer", "double", "character", "integer"))
+  expect_equal(c(sapply(res$data[1, ], typeof), use.names=F),
+               c("integer", "integer", "integer", "integer", "double", "integer", "double", "double", "logical", "integer", "character", "character", "integer", "character","integer", "integer", "double", "character", "integer"))
 })
+
+ 

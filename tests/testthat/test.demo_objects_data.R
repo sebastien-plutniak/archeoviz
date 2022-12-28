ef4e252fb185d0f56dbaf6cac7bbcf7d3c9ab067
sebastien-plutniak <sebastien.plutnia@posteo.net>
2022-12-28 11:16:23 +0100

    fix test

--- tests/testthat/test.demo_objects_data.R
+++ tests/testthat/test.demo_objects_data.R
@@ -2,7 +2,11 @@ test_that("demo_objects_data", {
 
   res <- demo_objects_data(100)
   
-  expect_equal(dim(res), c(100, 13))
-  expect_equal(colnames(res), c("id", "square_x", "square_y", "xmin", "xmax", "ymin", "ymax", "zmin", "zmax", "layer", "object_type", "object_class_size", "year"))
-  
+  expect_equal(dim(res), c(100, 14))
+  expect_equal(colnames(res), 
+               c("id", "square_x", "square_y", "xmin", "xmax", "ymin", "ymax", "zmin", "zmax", "layer", "object_type", "object_class_size", "year", "object_lithic_type"))
+  expect_equal(c(sapply(res, typeof), use.names=F), 
+               c("integer", "integer", "integer", "integer", "double", "integer", "double", "double", "logical", "integer", "character", "character", "integer", "character"))
 })
+
+
--- tests/testthat/test.do_objects_dataset.R
+++ tests/testthat/test.do_objects_dataset.R
@@ -5,10 +5,10 @@ test_that(".do_objects_dataset: simulated data", {
   res <- .do_objects_dataset(demoData.n = 100)
   
   expect_equal(res$notif.text, "notif.objects.ok")
-  expect_equal(dim(res$data), c(100, 19))
-  expect_equal(colnames(res$data), c("id", "square_x", "square_y", "xmin", "xmax", "ymin", "ymax", "zmin", "zmax", "layer", "object_type", "object_class_size", "year", "location_mode", "x", "y", "z", "square", "layer_color"))
+  expect_equal(dim(res$data), c(100, 20))
+  expect_equal(colnames(res$data), c("id", "square_x", "square_y", "xmin", "xmax", "ymin", "ymax", "zmin", "zmax", "layer", "object_type", "object_class_size", "year", "object_lithic_type", "location_mode", "x", "y", "z", "square", "layer_color"))
   expect_equal(c(sapply(res$data[1, ], typeof), use.names=F),
-               c("integer", "integer", "integer", "integer", "double", "integer", "double", "double", "logical", "integer", "character", "character", "integer", "character","integer", "integer", "double", "character", "integer"))
+               c("integer", "integer", "integer", "integer", "double", "integer", "double", "double", "logical", "integer", "character", "character", "integer", "character", "character", "integer", "integer", "double", "character", "integer"))
 })
 
  

17fe1e5e43bb8de3d2eeaa9235faeb1e216087d1
sebastien-plutniak <sebastien.plutnia@posteo.net>
2022-12-28 18:27:12 +0100

    handle missing coordinates

--- R/do_objects_dataset.R
+++ R/do_objects_dataset.R
@@ -12,7 +12,7 @@
   
   colnames(df) <- tolower(colnames(df))
   
-  #  file validation ----
+  #  Tests file ----
   
   required.fields <- c("id", "square_x", "square_y", "xmin", "ymin", "zmin", "layer", "object_type")
   
@@ -25,7 +25,7 @@
     notif.type <- "message"
   }
   
-  # test coords format ----
+  # : test coords format ----
   coords.type.check <- function(x){
     typeof(x) %in% c("integer", "double")
     }
@@ -36,38 +36,43 @@
                 notif.type = "error"))
   }
   
-  # test unique identifiers ----
+  # : test unique identifiers ----
   if(sum(duplicated(df$id)) > 0){
     return(list(data = df,
                 notif.text = "notif.error.identifier",
                 notif.type = "error"))
   }
+  # Coordinates ----
+  # : clean empty coordinates values
+  df[which(df$xmin == "" | df$xmin == " "), "xmin"] <- NA
+  df[which(df$ymin == "" | df$ymin == " "), "ymin"] <- NA
+  df[which(df$zmin == "" | df$zmin == " "), "zmin"] <- NA
   
-  # add max coordinates if absent: ----
+  # : add max coordinates if absent: ----
   if(is.null(df$xmax)){ df$xmax <- df$xmin }
   if(is.null(df$ymax)){ df$ymax <- df$ymin }
   if(is.null(df$zmax)){ df$zmax <- df$zmin }
   
-  # tag location mode:
+  # : location mode ----
   df$location_mode <- .term_switcher("exact")
   
-  # generate random coordinates if needed:
+  # : generate random coordinates if needed ----
   location.term <- .term_switcher("fuzzy")
   df <- .coordinates_sampling(df, "xmin", "xmax", "x", location.term)
   df <- .coordinates_sampling(df, "ymin", "ymax", "y", location.term)
   df <- .coordinates_sampling(df, "zmin", "zmax", "z", location.term)
   
-  # add square identifier:
+  # add square identifier ----
   df$square <- paste(df$square_x, df$square_y, sep = "-")
   
-  # order layers by mean depth:
-  sorted.layers <- by(df, df$layer, function(x) mean(x$z)  )
+  # order layers by mean depth ----
+  sorted.layers <- by(df, df$layer, function(x) mean(x$z, na.rm = TRUE)  )
   sorted.layers <- sapply(1:length(sorted.layers),
                           function(x) sorted.layers[x])
   sorted.layers <- sort(sorted.layers)
   df$layer <- factor(df$layer, levels = names(sorted.layers))
   
-  # add color:
+  # add color ----
   df$layer_color <- factor(df$layer,
                            levels = levels(df$layer),
                            labels = grDevices::rainbow(length(levels(df$layer))))

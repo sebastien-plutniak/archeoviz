fda7211ab1c8f85bc354f4bbae419408f9a00a70
sebastien-plutniak <sebastien.plutnia@posteo.net>
2023-02-26 14:58:02 +0100

    revise tables

--- R/do_by_layer_table.R
+++ R/do_by_layer_table.R
@@ -4,14 +4,15 @@
   if(ncol(df) > 1 & nrow(df) > 0){
     df <- as.matrix(df)
     df <- df[order(rownames(df), decreasing = FALSE), ]
-    df <- rbind(df, "Total" = apply(df, 2, sum))
-    df <- cbind(df, "Total" = apply(df, 1, sum))
+    df <- rbind(df, "total" = apply(df, 2, sum))
+    df <- cbind(df, "total" = apply(df, 1, sum))
   }
   if(ncol(df) == 1 & nrow(df) > 1){
     df <- df[order(rownames(df), decreasing = FALSE), ]
-    df <- c(df, "Total" = sum(df))
+    df <- c(df, "total" = sum(df))
     df <- as.data.frame(df)
     colnames(df) <- input.location
   }
+  colnames(df) <- sapply(colnames(df), .term_switcher)
   data.frame(df)
 }
--- R/do_by_variable_table.R
+++ R/do_by_variable_table.R
@@ -5,17 +5,20 @@
   if(nrow(df) > 1 & ncol(df) > 1){
     df <- as.matrix(df)
     df <- df[order(df[,1], decreasing = T), ]
-    df <- rbind(df, "Total" = apply(df, 2, sum))
-    df <- cbind(df, "Total" = apply(df, 1, sum))
+    df <- rbind(df, "total" = apply(df, 2, sum))
+    df <- cbind(df, "total" = apply(df, 1, sum))
   }
   
   if(nrow(df) > 1 & ncol(df) == 1){
     df <- df[order(df[,1], decreasing = T), ]
-    df <- c(df, "Total" = sum(df))
+    df <- c(df, "total" = sum(df))
     df <- as.data.frame(df)
     colnames(df) <- input.location
   } else {
     df <- as.data.frame.matrix(df)
   }
+  
+  colnames(df) <- sapply(colnames(df), .term_switcher)
+  
   df
 }

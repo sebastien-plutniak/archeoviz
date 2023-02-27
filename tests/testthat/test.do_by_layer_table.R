ba2add0549e67c1e4b9df9247b666263e738e954
sebastien-plutniak <sebastien.plutnia@posteo.net>
2023-02-27 01:47:55 +0100

    revise test tables

--- tests/testthat/test.do_by_layer_table.R
+++ tests/testthat/test.do_by_layer_table.R
@@ -1,21 +1,28 @@
 test_that(".do_by_layer_table: 1 location mode", {
-
+  ui.terms <- .load_interface_terms("fr")
+  shinyOptions("objects.df"  = df, "ui.terms" = ui.terms)
+  
   res <- demo_objects_data(100)
   res$location_mode <- "exact"
   res <- .do_by_layer_table(res, "exact")
   
   expect_equal(class(res), "data.frame")
+  expect_equal(colnames(res), "Exacte")
   expect_equal(typeof(res), "list")
   expect_true(is.integer(res[, 1]))
 })
 
 
 test_that(".do_by_layer_table: 2 location modes", {
+  ui.terms <- .load_interface_terms("en")
+  shinyOptions("objects.df"  = df, "ui.terms" = ui.terms)
+  
+  
   res <- demo_objects_data(100)
   res$location_mode <- sample(c("exact", "fuzzy"), nrow(res), replace = T)
   res <- .do_by_layer_table(res, c("exact", "fuzzy"))
   
   expect_equal(class(res), "data.frame")
-  expect_equal(colnames(res),  c("exact", "fuzzy", "Total"))
+  expect_equal(colnames(res),  c("Exact", "Fuzzy", "Total"))
   expect_equal(c(sapply(res[1,], typeof), use.names=F), c("integer", "integer", "integer"))
 })
--- tests/testthat/test.do_by_variable_table.R
+++ tests/testthat/test.do_by_variable_table.R
@@ -1,4 +1,6 @@
 test_that(".do_by_variable_table: 1 location mode", {
+  ui.terms <- .load_interface_terms("fr")
+  shinyOptions("objects.df"  = df, "ui.terms" = ui.terms)
   
   res <- demo_objects_data(100)
   res$location_mode <- "exact"
@@ -6,16 +8,20 @@ test_that(".do_by_variable_table: 1 location mode", {
   
   expect_equal(class(res), "data.frame")
   expect_equal(typeof(res), "list")
+  expect_equal(colnames(res),  "Exacte")
   expect_true(is.integer(res[, 1]))
 })
 
 
 test_that(".do_by_variable_table: 2 location modes", {
+  ui.terms <- .load_interface_terms("fr")
+  shinyOptions("objects.df"  = df, "ui.terms" = ui.terms)
+  
   res <- demo_objects_data(100)
   res$location_mode <- sample(c("exact", "fuzzy"), nrow(res), replace = T)
   res <- .do_by_variable_table(res, "object_type", c("exact", "fuzzy"))
   
   expect_equal(class(res), "data.frame")
-  expect_equal(colnames(res),  c("exact", "fuzzy", "Total"))
+  expect_equal(colnames(res),  c("Exacte", "Vague", "Total"))
   expect_true(is.integer(res[, 3]))
 })

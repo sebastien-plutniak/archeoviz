e70dfee621467cd01681efe55f91a047bedfe817
sebastien-plutniak <sebastien.plutnia@posteo.net>
2023-06-04 08:04:07 +0200

    add .do_square_list()

---
+++ R/do_square_list.R
@@ -0,0 +1,15 @@
+.do_square_list <- function(square.size, coords.min, coords.max, square.list, axes){
+  
+  max.nr.of.squares <- length(unique(trunc(seq(coords.min, coords.max) / square.size) * square.size)) - 1
+  square.labels <- levels(square.list)
+  
+  if(max.nr.of.squares == length(square.labels) ){
+    squares <- square.labels
+  }  else {
+    message(paste0(max.nr.of.squares, " ", axes, " squares, but ",
+                   length(square.labels), " labels provided: ", 
+                   paste0(square.labels, collapse = ", "), ".\n"))
+    squares <- "" # if the nr of square labels is insufficient, do not display the labels
+  }
+  squares
+}

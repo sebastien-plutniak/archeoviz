3243db4c83438d699f0c6db23be05adcf4c1f06b
sebastien-plutniak <sebastien.plutnia@posteo.net>
2024-04-05 01:34:35 +0200

    round coords and convert to integer

--- R/rotate.R
+++ R/rotate.R
@@ -3,6 +3,5 @@
     rotated.mat <- matrix(c(cos(radians), -sin(radians), sin(radians), cos(radians)), 
                    byrow = TRUE, ncol = 2)
     rotated.mat <- t(pivot + rotated.mat %*% (t(as.matrix(coords) ) - pivot))
-    apply(rotated.mat, 2, as.integer)
-    rotated.mat
+    apply(rotated.mat, 2, function(x) as.integer(round(x)))
 }

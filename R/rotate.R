b5ffd836bceb2d3901b082aff3a7bf41a6ee47bb
sebastien-plutniak <sebastien.plutnia@posteo.net>
2024-04-07 05:49:03 +0200

    remove conersion to integer

--- R/rotate.R
+++ R/rotate.R
@@ -2,6 +2,5 @@
     radians <- - degrees * pi / 180  
     rotated.mat <- matrix(c(cos(radians), -sin(radians), sin(radians), cos(radians)), 
                    byrow = TRUE, ncol = 2)
-    rotated.mat <- t(pivot + rotated.mat %*% (t(as.matrix(coords) ) - pivot))
-    apply(rotated.mat, 2, function(x) as.integer(round(x)))
+    t(pivot + rotated.mat %*% (t(as.matrix(coords) ) - pivot))
 }
